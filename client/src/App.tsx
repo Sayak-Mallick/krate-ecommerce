f32aecaaa093d811dee0f1215c78e97a7dbc4615
Sayak-Mallick <sayakmallickkv@gmail.com>
2026-04-02 06:14:23 +0530

    [TASK] : Installed necessary dependencies

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,18 @@
-import './App.css'
+import "./App.css";
 
 function App() {
-
   return (
     <>
       <div>
-          <h1>Krate- Ecommerce</h1>
+        <h1>Krate- Ecommerce</h1>
+        <div className="flex min-h-svh flex-col items-center justify-center">
+          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+            Click me
+          </button>
+        </div>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
