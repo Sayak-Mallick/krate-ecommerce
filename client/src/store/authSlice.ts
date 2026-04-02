43d1cdf3917e3b28d2f0fd71bc08868c167d587b
Sayak-Mallick <sayakmallickkv@gmail.com>
2026-04-02 06:31:29 +0530

    [TASK] : Initialized Redux Toolkit

---
+++ client/src/store/authSlice.ts
@@ -0,0 +1,28 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+interface AuthState {
+  isAuthenticated: boolean;
+}
+
+
+const initialState: AuthState = {
+  isAuthenticated: false,
+};
+
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+   setUser:() => {
+    // state.isAuthenticated = true;
+   }
+  }, 
+});
+
+export const {setUser} =authSlice.actions;
+export default authSlice.reducer;
+
+
+
+
