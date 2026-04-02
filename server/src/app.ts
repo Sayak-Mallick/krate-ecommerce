d2549de7176cd7f7ce1da5aa170db9b7eaf9c9e9
Sayak-Mallick <sayakmallickkv@gmail.com>
2026-04-02 06:06:18 +0530

    [TASK] : Configured cookie-parser

--- server/src/app.ts
+++ server/src/app.ts
@@ -7,7 +7,20 @@ const app: Application = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+    allowedHeaders: [
+      "Content-Type",
+      "Authorization",
+      "Cache-Control",
+      "Expires",
+      "Pragma",
+    ],
+    credentials: true,
+  })
+);
 
 app.get("/health", (req, res) => {
   res.json({
